50e42c23871bd70befbb2e839a3626567f0941c9
mateuszkepczynski <mateuszkepes@outlook.com>
2019-10-23 11:18:19 +0200

    Few changes.

---
+++ app/src/main/java/pl/pjatk/jaz/login/UserSession.java
@@ -0,0 +1,20 @@
+package pl.pjatk.jaz.login;
+
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+@SessionScoped
+public class UserSession implements Serializable
+{
+    private String state;
+
+    public String getState()
+    {
+        return state;
+    }
+
+    public void setState(String state)
+    {
+        this.state = state;
+    }
+}
