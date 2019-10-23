e94fc0ba52b2ee1a087e30f131580fcac184d378
mateuszkepczynski <mateuszkepes@outlook.com>
2019-10-23 11:17:28 +0200

    Few changes.

--- app/src/main/java/pl/pjatk/jaz/login/JavFilter.java
+++ app/src/main/java/pl/pjatk/jaz/login/JavFilter.java
@@ -1,21 +1,32 @@
 package pl.pjatk.jaz.login;
 
+import javax.inject.Inject;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebFilter("/*")
 public class JavFilter extends HttpFilter
 {
-        /*
-        if(req.getSession() != null)
+    @Inject
+    private UserSession userSession;
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException
+    {
+        if(req.getRequestURI().contains("/login"))
         {
-            res.sendRedirect("http://google.pl");
+            userSession.setState("123123ee");
+            res.sendRedirect("http://google.com");
+        }
+        else
+        {
+            System.out.println("ww");
+            chain.doFilter(req,res);
         }
     }
-            if (req.getRequestURI().contains("/login"))
-            {
-                res.sendRedirect("http://google.pl");
-                return;
-            }
-         */
 }
