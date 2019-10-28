341631d2cdf3898ff59a9907e690ffdc5415895f
mateuszkepczynski <mateuszkepes@outlook.com>
2019-10-28 11:11:48 +0100

    Improved front-end, and session bean etc.

--- app/src/main/java/pl/pjatk/jaz/AverageServlet.java
+++ app/src/main/java/pl/pjatk/jaz/AverageServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("hello")
+@WebServlet("hello") //test
 public class AverageServlet extends HttpServlet
 {
     @Override
